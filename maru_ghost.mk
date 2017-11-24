2572d56e53803801d84d5be868e32f193dbc372c
root <root@jenkins-miui.c.calcium-branch-186802.internal>
2017-11-24 02:57:12 +0000

    correct integration with maru

--- maru_ghost.mk
+++ maru_ghost.mk
@@ -15,7 +15,7 @@
 # limitations under the License.
 #
 $(call inherit-product, device/motorola/ghost/full_ghost.mk)
-$(call inherit-product, vendor/maruos/device-ghost.mk)
+$(call inherit-product, vendor/maruos/device-maru.mk)
 $(call inherit-product, vendor/maruos/BoardConfigVendor.mk)
 
 PRODUCT_NAME := maru_ghost
